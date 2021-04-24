86ab4724fc7d8473dc60a54a525e476887cf62d0
Eren <eren@redmc.me>
2021-04-24 12:05:50 +0300

    format

--- src/Eren5960/DebugCommand/Main.php
+++ src/Eren5960/DebugCommand/Main.php
@@ -10,21 +10,30 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 use pocketmine\utils\MainLogger;
 
-class Main extends PluginBase{
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
-		$mode = array_shift($args) ?? 'anything';
-		if(!in_array($mode, ['on', 'off'])){
-			$sender->sendMessage('Usage: /debug <on/off>');
-			return false;
-		}
+class Main extends PluginBase {
+    public function onCommand(
+        CommandSender $sender,
+        Command $command,
+        string $label,
+        array $args
+    ): bool {
+        $mode = array_shift($args) ?? 'anything';
+        if (!in_array($mode, ['on', 'off'])) {
+            $sender->sendMessage('Usage: /debug <on/off>');
+            return false;
+        }
 
-		$logger = Server::getInstance()->getLogger();
-		if($logger instanceof MainLogger){
-			$logger->setLogDebug($mode === 'on');
-			$sender->sendMessage('Debug mode is ' . ($mode === 'on' ? 'enabled' : 'disabled') . '.');
-		}else{
-			$sender->sendMessage('Unexpected Logger: ' . get_class($logger));
-		}
-		return true;
-	}
+        $logger = Server::getInstance()->getLogger();
+        if ($logger instanceof MainLogger) {
+            $logger->setLogDebug($mode === 'on');
+            $sender->sendMessage(
+                'Debug mode is ' .
+                    ($mode === 'on' ? 'enabled' : 'disabled') .
+                    '.'
+            );
+        } else {
+            $sender->sendMessage('Unexpected Logger: ' . get_class($logger));
+        }
+        return true;
+    }
 }
